73febddfbe571a3e581b4713d875a228dead1351
nazilaabedi10 <nazyla_abedi@outlook.com>
2025-02-02 12:27:46 -0800

    Add files via upload

--- player.c
+++ player.c
@@ -18,10 +18,8 @@ int placePlayer(Room**rooms, Player*user){
     user->position->x=rooms[3]->position.x+1;
     user->position->y=rooms[3]->position.y+1;
 
-    mvprintw(user->position->y, user->position->x, "@");
-    move(user->position->y, user->position->x);
 }
-int handleInput(int input, Player*user){
+Position* handleInput(int input, Player*user){
 
     Position*newPosition;
     newPosition=malloc(sizeof(Position));
@@ -85,27 +83,36 @@ int handleInput(int input, Player*user){
             break;
         
     }
-    checkPosition(newY, newX, user);
+    checkPosition(Position*newPosition, Level*level);
 }
 //check what is at next position
-int checkPosition(int newY, int newX, Player*user){
+int checkPosition(Position*newPosition, Level*level){
+    Player*user;
+    user=level->user;
     int space;
-    switch(mvinch(newY,newX)){
+    switch(mvinch(newPosition->y,newPosition->x)){
         case '.':
         case '#':
         case '+':
-            playerMove(newY,newX, user);
+            playerMove(newPosition, user, level->tiles);
             break;
+        case 'X':
+        case 'G':
+        case 'T':
+            combat(user,getMonsterAt(newPosition,level->monsters),1);
         default:
-            move(user->position.y, user->position.x);
             break;
     }
 }
 
-int playerMove(int y, int x, Player*user){
-    mvprintw(user->position.y, user->position.x, ".");
-    user->position.y=y;
-    user->position.x=x;
-    mvprintw(user->position.y, user->position.x, "@");
-    move(user->position.y, user->position.x);
+int playerMove(Position*newPosition, Player*user, char**level){
+    char buffer[8];
+
+    sprintf(buffer, "%c", level[user->position->y][user->position->x]);
+
+    user->position->y=newPosition->y;
+    user->position->x=newPosition->x;
+
+    mvprintw(user->position->y, user->position->x, "@");
+    move(user->position->y, user->position->x);
 }
